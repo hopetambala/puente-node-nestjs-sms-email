1063c41051f1cf62076d90919db46f2667394fb0
Hope Tambala <hopetambala@outlook.com>
2022-06-11 16:19:55 -0400

    chore: initial signup create

--- src/email/signup.controller.ts
+++ src/email/signup.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { EmailService } from './service/email.service';
-import { Message } from './dto/message';
+import { Message, RestPostCall } from './dto/message';
 
 @Controller('signup')
 export class SignupController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post()
-  create(@Body() createEmailDto: Message) {
-    return this.emailService.create(createEmailDto);
+  create(@Body() data: RestPostCall) {
+    const { emailSubject:subject, emailBody:text, emailsToSendTo } = data
+    const to = emailsToSendTo.map((email)=>({
+        email,
+        type:"to"
+    }))
+    
+    return this.emailService.create({
+      from_email:"tech@puente-dr.org",
+      subject,
+      text,
+      to}).catch((e: any)=>{
+        return `INVALID${e}`
+      });
   }
 
   @Get()
