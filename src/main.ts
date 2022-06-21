5ee4c2e1009601c4fff902d9a5820caf794dbc2a
Hope Tambala <hopetambala@outlook.com>
2022-06-20 22:21:23 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env)
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
