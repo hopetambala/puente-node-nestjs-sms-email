ac7cc1cd48f8b142f67ad583d1417e24ee71b34a
Hope Tambala <hopetambala@outlook.com>
2022-09-04 13:25:10 -0400

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT || 4000;
   await app.listen(port);
 
